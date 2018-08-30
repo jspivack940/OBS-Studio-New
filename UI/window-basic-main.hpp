d33ce6f3196ae79173ed2db35bf4be45ff50f562
Andersama <anderson.john.alexander@gmail.com>
2018-08-30 04:58:18 +0200

    UI/libobs: Add Master Mixer
    This adds a new dock with vue-meters and faders for all tracks, which
    will allow to monitor clippings for outputs as well as change their
    volume.
    A particular attention has been given to UX :
    - capability to apply filters to each track;
    - capability to monitor each track;
    - hotkeys to mute/unmute, monitor/unmonitor;
    - additional buttons to enable/disable tracks for streaming/record;
    (as a shortcut to Settings > Output)
    (while keeping the other buttons of source mixer - mute, advanced audio
    properties, hide, unhide)
    - display of master mixer is synced to the output mode (simple/advanced)
    (in simple mode, only track 1 is shown)
    - to unclutter front-end for beginners, the master mixer is hidden on
    default settings.
    - master mixer is vertical on default to be closer to customary display
    in most DAWs.
    
    See : https://ideas.obsproject.com/ideas/170/output-volume-visualization
    https://obsproject.com/forum/threads/master-audio-level-meters.78608/#post-332745
    Closes https://obsproject.com/mantis/view.php?id=405
    
    Thanks to SuslikV and WizardCM for feedback.
    
    Co-authored-by: Andersama <anderson.john.alexander@gmail.com>
    Co-authored-by: pkv <pkv.stream@gmail.com>
    Co-authored-by: Shaolin <admshao@gmail.com>

--- UI/window-basic-main.hpp
+++ UI/window-basic-main.hpp
@@ -144,7 +144,7 @@ private:
 	std::shared_ptr<Auth> auth;
 
 	std::vector<VolControl*> volumes;
-
+	std::vector<VolControl*> master_volumes;
 	std::vector<OBSSignal> signalHandlers;
 
 	QList<QPointer<QDockWidget>> extraDocks;
@@ -246,6 +246,7 @@ private:
 	void          UpdateVolumeControlsDecayRate();
 	void          UpdateVolumeControlsPeakMeterType();
 	void          ClearVolumeControls();
+	void          ClearMasterVolumeControls();
 
 	void          UploadLog(const char *subdir, const char *file);
 
@@ -303,8 +304,10 @@ private:
 	void GetAudioSourceFilters();
 	void GetAudioSourceProperties();
 	void VolControlContextMenu();
+	void MasterVolControlContextMenu();
 	void ToggleVolControlLayout();
-	void ToggleMixerLayout(bool vertical);
+	void ToggleMasterVolControlLayout();
+	void ToggleMixerLayout(bool vertical, bool isMaster);
 
 	void RefreshSceneCollections();
 	void ChangeSceneCollection();
@@ -503,12 +506,16 @@ private slots:
 
 	void HideAudioControl();
 	void UnhideAllAudioControls();
+	void UnhideAllMasterAudioControls();
+
 	void ToggleHideMixer();
 
 	void MixerRenameSource();
 
 	void on_vMixerScrollArea_customContextMenuRequested();
 	void on_hMixerScrollArea_customContextMenuRequested();
+	void on_vMasterMixerScrollArea_customContextMenuRequested();
+	void on_hMasterMixerScrollArea_customContextMenuRequested();
 
 	void on_actionCopySource_triggered();
 	void on_actionPasteRef_triggered();
@@ -556,7 +563,8 @@ private:
 public:
 	OBSSource GetProgramSource();
 	OBSScene GetCurrentScene();
-
+	void InitAudioMasterMixer();
+	inline std::vector<VolControl*> const GetMasterVol() { return master_volumes; }
 	void SysTrayNotify(const QString &text, QSystemTrayIcon::MessageIcon n);
 
 	inline OBSSource GetCurrentSceneSource()
@@ -796,6 +804,7 @@ private slots:
 	void DeferredLoad(const QString &file, int requeueCount);
 
 	void StackedMixerAreaContextMenuRequested();
+	void StackedMasterMixerAreaContextMenuRequested();
 
 	void ResizeOutputSizeOfSource();
 
