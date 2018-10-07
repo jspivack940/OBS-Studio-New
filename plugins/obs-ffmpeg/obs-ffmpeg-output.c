f7f9ee445206fda36941f5402927c1f5aabd318e
pkviet <pkv.stream@gmail.com>
2018-10-07 22:27:56 +0200

    libobs/media-io: Expand surround sound capabilities
    Adds support for most common channel layouts in FFmpeg, 3.0, 3.1,
    quad, 6.0, 6.1, 7.0, octagonal = 8.0, 9.0, 10., 11.0, 12.0 up to 16.0.
    They will be supported by FFmpeg native aac encoder only if using
    FFmpeg master (through pce, program configuration element).

--- plugins/obs-ffmpeg/obs-ffmpeg-output.c
+++ plugins/obs-ffmpeg/obs-ffmpeg-output.c
@@ -312,17 +312,13 @@ static bool create_audio_stream(struct ffmpeg_data *data, int idx)
 		return false;
 
 	data->audio_streams[idx] = stream;
+
 	context = data->audio_streams[idx]->codec;
 	context->bit_rate = (int64_t)data->config.audio_bitrate * 1000;
 	context->time_base = (AVRational){1, aoi.samples_per_sec};
 	context->channels = get_audio_channels(aoi.speakers);
 	context->sample_rate = aoi.samples_per_sec;
-	context->channel_layout =
-		av_get_default_channel_layout(context->channels);
-
-	//AVlib default channel layout for 5 channels is 5.0 ; fix for 4.1
-	if (aoi.speakers == SPEAKERS_4POINT1)
-		context->channel_layout = av_get_channel_layout("4.1");
+	context->channel_layout = convert_speaker_layout(aoi.speakers);
 
 	context->sample_fmt = data->acodec->sample_fmts
 				      ? data->acodec->sample_fmts[0]
