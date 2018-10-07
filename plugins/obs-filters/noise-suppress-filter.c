5c08b02c79778612f5283d8e1370e6d04e918a78
pkviet <pkv.stream@gmail.com>
2018-10-07 22:27:56 +0200

    libobs/media-io: Expand surround sound capabilities
    Adds support for most common channel layouts in FFmpeg, 3.0, 3.1,
    quad, 6.0, 6.1, 7.0, octagonal = 8.0, 9.0, 10., 11.0, 12.0 up to 16.0.
    They will be supported by FFmpeg native aac encoder only if using
    FFmpeg master (through pce, program configuration element).

--- plugins/obs-filters/noise-suppress-filter.c
+++ plugins/obs-filters/noise-suppress-filter.c
@@ -44,7 +44,7 @@
 #define TEXT_METHOD_SPEEX MT_("NoiseSuppress.Method.Speex")
 #define TEXT_METHOD_RNN MT_("NoiseSuppress.Method.RNNoise")
 
-#define MAX_PREPROC_CHANNELS 8
+#define MAX_PREPROC_CHANNELS 16
 
 /* RNNoise constants, these can't be changed */
 #define RNNOISE_SAMPLE_RATE 48000
