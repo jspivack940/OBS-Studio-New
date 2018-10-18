9e5cf35fb6104dbad2adac29929a13d912da317a
pkviet <pkv.stream@gmail.com>
2018-10-18 02:33:51 +0200

    UI: Add tracks buttons to source meters
    Setting the tracks for an audio source is done through the Advanced
    Audio Panel, which is not very handy. This commit adds track selection
    buttons to the source meter.
    The tracks buttons are hidden on default and are displayed through the
    mixer context menu.

--- UI/window-basic-main.cpp
+++ UI/window-basic-main.cpp
@@ -3075,6 +3075,11 @@ void OBSBasic::VolControlContextMenu()
 	ShowMonitoringButtonAction.setChecked(config_get_bool(
 		GetGlobalConfig(), "BasicWindow", "ShowMonitoringButton"));
 
+	QAction showTracksButtonsAction(QTStr("ShowTracksButtons"), this);
+	showTracksButtonsAction.setCheckable(true);
+	showTracksButtonsAction.setChecked(config_get_bool(
+		GetGlobalConfig(), "BasicWindow", "ShowTracksButtons"));
+
 	/* ------------------- */
 
 	connect(&hideAction, &QAction::triggered, this,
@@ -3106,6 +3111,9 @@ void OBSBasic::VolControlContextMenu()
 
 	connect(&ShowMonitoringButtonAction, &QAction::changed, this,
 		&OBSBasic::ShowMonitoringButton, Qt::DirectConnection);
+
+	connect(&showTracksButtonsAction, &QAction::changed, this,
+		&OBSBasic::ShowTracksButtons, Qt::DirectConnection);
 
 	/* ------------------- */
 
@@ -3145,6 +3153,7 @@ void OBSBasic::VolControlContextMenu()
 	popup.addSeparator();
 	popup.addAction(&toggleControlLayoutAction);
 	popup.addAction(&ShowMonitoringButtonAction);
+	popup.addAction(&showTracksButtonsAction);
 	popup.addSeparator();
 	popup.addAction(&filtersAction);
 	popup.addAction(&propertiesAction);
@@ -3188,6 +3197,11 @@ void OBSBasic::StackedMixerAreaContextMenuRequested()
 	ShowMonitoringButtonAction.setChecked(config_get_bool(
 		GetGlobalConfig(), "BasicWindow", "ShowMonitoringButton"));
 
+	QAction showTracksButtonsAction(QTStr("ShowTracksButtons"), this);
+	showTracksButtonsAction.setCheckable(true);
+	showTracksButtonsAction.setChecked(config_get_bool(
+		GetGlobalConfig(), "BasicWindow", "ShowTracksButtons"));
+
 	/* ------------------- */
 
 	connect(&unhideAllAction, &QAction::triggered, this,
@@ -3205,6 +3219,9 @@ void OBSBasic::StackedMixerAreaContextMenuRequested()
 	connect(&ShowMonitoringButtonAction, &QAction::changed, this,
 		&OBSBasic::ShowMonitoringButton, Qt::DirectConnection);
 
+	connect(&showTracksButtonsAction, &QAction::changed, this,
+		&OBSBasic::ShowTracksButtons, Qt::DirectConnection);
+
 	/* ------------------- */
 
 	QMenu popup;
@@ -3212,6 +3229,7 @@ void OBSBasic::StackedMixerAreaContextMenuRequested()
 	popup.addSeparator();
 	popup.addAction(&toggleControlLayoutAction);
 	popup.addAction(&ShowMonitoringButtonAction);
+	popup.addAction(&showTracksButtonsAction);
 	popup.addSeparator();
 	popup.addAction(&advPropAction);
 	popup.exec(QCursor::pos());
@@ -3304,6 +3322,18 @@ void OBSBasic::ShowMonitoringButton()
 	}
 }
 
+void OBSBasic::ShowTracksButtons()
+{
+	bool showTracks = !config_get_bool(GetGlobalConfig(), "BasicWindow",
+					   "ShowTracksButtons");
+	config_set_bool(GetGlobalConfig(), "BasicWindow", "ShowTracksButtons",
+			showTracks);
+
+	for (auto volume : volumes) {
+		volume->showTracksButtons(showTracks);
+	}
+}
+
 void OBSBasic::ToggleMasterVolControlLayout()
 {
 	bool vertical = !config_get_bool(GetGlobalConfig(), "BasicWindow",
@@ -3324,8 +3354,11 @@ void OBSBasic::ActivateAudioSource(OBSSource source)
 					"VerticalVolControl");
 	bool ShowMonitoringButton = config_get_bool(
 		GetGlobalConfig(), "BasicWindow", "ShowMonitoringButton");
+	bool ShowTracksButtons = config_get_bool(
+		GetGlobalConfig(), "BasicWindow", "ShowTracksButtons");
 	VolControl *vol = new VolControl(source, NULL, true, vertical,
 					 ShowMonitoringButton,
+					 ShowTracksButtons,
 					 SOURCE_IS_NOT_TRACK);
 
 	vol->EnableSlider(!SourceVolumeLocked(source));
@@ -3387,7 +3420,7 @@ void OBSBasic::InitAudioMasterMixer()
 	bool hidden[MAX_AUDIO_MIXES];
 	for (int i = 0; i < MAX_AUDIO_MIXES; i++) {
 		vol[i] = new VolControl(tracks[i], &muted[i], true, vertical,
-					true, i);
+					true, false, i);
 		meters[i] = vol[i]->GetMeter();
 		faders[i] = vol[i]->GetFader();
 		std::string trackNum = "Track" + std::to_string(i + 1) + "Name";
