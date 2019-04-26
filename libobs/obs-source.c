a4dbc9ace932627f58dc83ece6789d8218f12391
pkv <pkv.stream@gmail.com>
2019-04-26 07:32:46 +0200

    libobs: Add signal for monitoring

--- libobs/obs-source.c
+++ libobs/obs-source.c
@@ -101,6 +101,7 @@ static const char *source_signals[] = {
 	"void media_previous(ptr source)",
 	"void media_started(ptr source)",
 	"void media_ended(ptr source)",
+	"void monitoring_type(ptr source, int mon_type)",
 	NULL,
 };
 
@@ -4694,12 +4695,24 @@ void obs_source_set_monitoring_type(obs_source_t *source,
 {
 	bool was_on;
 	bool now_on;
+	int index;
+	struct calldata data;
+	uint8_t stack[128];
 
 	if (!obs_source_valid(source, "obs_source_set_monitoring_type"))
 		return;
+
+	calldata_init_fixed(&data, stack, sizeof(stack));
+	calldata_set_ptr(&data, "source", source);
+	calldata_set_int(&data, "mon_type", type);
+
+	signal_handler_signal(source->context.signals, "monitoring_type",
+			      &data);
+
+	type = (int)calldata_int(&data, "mon_type");
+
 	if (source->monitoring_type == type)
 		return;
-
 	was_on = source->monitoring_type != OBS_MONITORING_TYPE_NONE;
 	now_on = type != OBS_MONITORING_TYPE_NONE;
 
@@ -4711,7 +4724,6 @@ void obs_source_set_monitoring_type(obs_source_t *source,
 			source->monitor = NULL;
 		}
 	}
-
 	source->monitoring_type = type;
 }
 
